34dc2d4ed549a99c9ab4d6b12b686c239b28694c
JonMK-maker <bretmclaughlin440@gmail.com>
2026-02-14 01:14:22 +0000

    fix: remove deprecated sentry options

--- next.config.ts
+++ next.config.ts
@@ -18,12 +18,6 @@ export default withSentryConfig(nextConfig, {
     // For all available options, see:
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
     // This can increase your server load as well as your hosting bill.
     // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
